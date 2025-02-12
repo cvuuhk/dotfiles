b29690063d5d933a68c931ac00f3b58905ac2142
崔书豪 <568126480@qq.com>
2025-02-12 21:07:56 +0800

    ranger general text

--- config/ranger/scope.sh
+++ config/ranger/scope.sh
@@ -293,7 +293,7 @@ handle_mime() {
             exit 1;;
 
         ## Text
-        text/* | message/* | */xml)
+        text/* | message/* | */xml | */vnd.gentoo.manifest)
             ## Syntax highlight
             if [[ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
                 exit 2
