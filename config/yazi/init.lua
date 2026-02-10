c7c3f43d64ed7ccefffe51812c20f4d8702ff8ef
崔书豪 <568126480@qq.com>
2026-02-10 19:03:01 +0800

    yazi 关闭边框

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,6 +1,6 @@
-require("full-border"):setup{
-    type = ui.Border.PLAIN
-}
+-- require("full-border"):setup{
+--     type = ui.Border.PLAIN
+-- }
 require("smart-enter"):setup {
 	open_multi = true,
 }
